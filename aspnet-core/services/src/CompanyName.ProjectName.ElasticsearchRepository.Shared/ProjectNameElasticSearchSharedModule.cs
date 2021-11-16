e7d7523114fada6e962d23e27f89f325bebee34b
王军 <510423039@qq.com>
2021-11-16 15:18:47 +0800

    ✨  删除多余ES类库

--- aspnet-core/services/src/CompanyName.ProjectName.ElasticsearchRepository.Shared/ProjectNameElasticSearchSharedModule.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Volo.Abp.Modularity;
-
-namespace CompanyName.ProjectName.ElasticsearchRepository.Shared
-{
-    public class ProjectNameElasticSearchSharedModule : AbpModule
-    {
-    }
-}
