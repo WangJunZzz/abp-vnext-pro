473d67f8a392487252adf92905dbb12ca126c858
shuangbaojun <190663855@qq.com>
2021-12-11 00:07:34 +0800

    基本布局功能实现

---
+++ aspnet-core/services/src/Lion.AbpPro.Blazor.Layout.AntDesignTheme/Pages/Identity/Roles/Role.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lion.AbpPro.Blazor.Layout.AntDesignTheme.Pages.Identity.Roles
+{
+    public partial class Role
+    {
+
+    }
+}
---
+++ aspnet-core/services/src/Lion.AbpPro.Blazor.Layout.AntDesignTheme/Pages/Identity/Users/User.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lion.AbpPro.Blazor.Layout.AntDesignTheme.Pages.Identity.Users
+{
+    public partial class User
+    {
+
+    }
+}
