a0779cff1d10a627591ac4cbdb262234dbbb7d82
shuangbaojun <190663855@qq.com>
2021-12-12 16:56:18 +0800

    :sparkles: 头部组件功能实现

--- aspnet-core/services/src/Lion.AbpPro.Blazor.Layout.AntDesignTheme/AbpProBlazorLayoutAntDesignThemeModule.cs
+++ aspnet-core/services/src/Lion.AbpPro.Blazor.Layout.AntDesignTheme/AbpProBlazorLayoutAntDesignThemeModule.cs
@@ -5,13 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.AspNetCore.Components.Web.Theming.Routing;
+using Volo.Abp.Autofac.WebAssembly;
 using Volo.Abp.Modularity;
 
 namespace Lion.AbpPro.Blazor.Layout.AntDesignTheme
 {
-    public class AbpProBlazorLayoutAntDesignThemeModule: AbpModule
+    [DependsOn(
+           typeof(AbpAutofacWebAssemblyModule)
+    )]
+    public class AbpProBlazorLayoutAntDesignThemeModule : AbpModule
     {
-
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             context.Services.AddAntDesign();
