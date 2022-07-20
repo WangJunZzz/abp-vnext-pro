1fc71cc7dd3c0fe338eefcb12dced28b03cac552
WangJunZzz <510423039@qq.com>
2022-07-20 21:08:54 +0800

    🚑 Vben 分页组件参数设置 #59

--- vben28/src/settings/componentSetting.ts
+++ vben28/src/settings/componentSetting.ts
@@ -9,13 +9,13 @@ export default {
     // support xxx.xxx.xxx
     fetchSetting: {
       // The field name of the current page passed to the background
-      pageField: 'page',
+      pageField: 'pageIndex',
       // The number field name of each page displayed in the background
       sizeField: 'pageSize',
       // Field name of the form data returned by the interface
       listField: 'items',
       // Total number of tables returned by the interface field name
-      totalField: 'total',
+      totalField: 'totalCount',
     },
     // Number of pages that can be selected
     pageSizeOptions: ['10', '50', '80', '100'],
