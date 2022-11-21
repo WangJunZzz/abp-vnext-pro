23c93e3b35b185c8c5574589e8ced180178c01ea
王军 <510423039@qq.com>
2022-11-21 16:23:27 +0800

    feat: 添加邮件模块

--- aspnet-core/services/src/Lion.AbpPro.Domain/Settings/AbpProSettingDefinitionProvider.cs
+++ aspnet-core/services/src/Lion.AbpPro.Domain/Settings/AbpProSettingDefinitionProvider.cs
@@ -4,17 +4,59 @@ namespace Lion.AbpPro.Settings
     {
         public override void Define(ISettingDefinitionContext context)
         {
-            //Define your own settings here. Example:
-            //context.Add(new SettingDefinition(AbpProSettings.MySetting1));
-            OverrideDefalutSettings(context);
+            ConfigEmail(context);
         }
 
-        /// <summary>
-        /// 重写默认setting添加自定义属性
-        /// </summary>
-        private static void OverrideDefalutSettings(ISettingDefinitionContext context)
+       private static void ConfigEmail(ISettingDefinitionContext context)
         {
+            context.GetOrNull(EmailSettingNames.Smtp.Host)
+                .WithProperty(AbpProSettings.Group.Default,
+                    AbpProSettings.Group.EmailManagement)
+                .WithProperty(AbpProSettings.ControlType.Default,
+                    AbpProSettings.ControlType.TypeText);
+
+            context.GetOrNull(EmailSettingNames.Smtp.Port)
+                .WithProperty(AbpProSettings.Group.Default,
+                    AbpProSettings.Group.EmailManagement)
+                .WithProperty(AbpProSettings.ControlType.Default,
+                    AbpProSettings.ControlType.Number);
+
+            context.GetOrNull(EmailSettingNames.Smtp.UserName)
+                .WithProperty(AbpProSettings.Group.Default,
+                    AbpProSettings.Group.EmailManagement)
+                .WithProperty(AbpProSettings.ControlType.Default,
+                    AbpProSettings.ControlType.TypeText);
+
+            context.GetOrNull(EmailSettingNames.Smtp.Password)
+                .WithProperty(AbpProSettings.Group.Default,
+                    AbpProSettings.Group.EmailManagement)
+                .WithProperty(AbpProSettings.ControlType.Default,
+                    AbpProSettings.ControlType.TypeText);
             
+
+            context.GetOrNull(EmailSettingNames.Smtp.EnableSsl)
+                .WithProperty(AbpProSettings.Group.Default,
+                    AbpProSettings.Group.EmailManagement)
+                .WithProperty(AbpProSettings.ControlType.Default,
+                    AbpProSettings.ControlType.TypeCheckBox);
+
+            context.GetOrNull(EmailSettingNames.Smtp.UseDefaultCredentials)
+                .WithProperty(AbpProSettings.Group.Default,
+                    AbpProSettings.Group.EmailManagement)
+                .WithProperty(AbpProSettings.ControlType.Default,
+                    AbpProSettings.ControlType.TypeCheckBox);
+
+            context.GetOrNull(EmailSettingNames.DefaultFromAddress)
+                .WithProperty(AbpProSettings.Group.Default,
+                    AbpProSettings.Group.EmailManagement)
+                .WithProperty(AbpProSettings.ControlType.Default,
+                    AbpProSettings.ControlType.TypeText);
+            
+            context.GetOrNull(EmailSettingNames.DefaultFromDisplayName)
+                .WithProperty(AbpProSettings.Group.Default,
+                    AbpProSettings.Group.EmailManagement)
+                .WithProperty(AbpProSettings.ControlType.Default,
+                    AbpProSettings.ControlType.TypeText);
         }
 
         private static LocalizableString L(string name)
