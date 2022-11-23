d3965c4d735add0f95cf2785444726cfcf6d7640
王军 <510423039@qq.com>
2022-11-23 18:19:43 +0800

    feat: 程序启动时，预热

--- aspnet-core/services/host/Lion.AbpPro.HttpApi.Host/Startup.cs
+++ aspnet-core/services/host/Lion.AbpPro.HttpApi.Host/Startup.cs
@@ -2,15 +2,35 @@ namespace Lion.AbpPro
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplication<AbpProHttpApiHostModule>();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app,IHostApplicationLifetime lifetime)
         {
             app.InitializeApplication();
-          
+            lifetime.ApplicationStopped.Register(ConfigurePreheat);
+        }
+        
+        
+        /// <summary>
+        /// 程序首次访问接口速度慢，事先预热
+        /// </summary>
+        private void ConfigurePreheat()
+        {
+            var url = _configuration.GetValue<string>("App:SelfUrl");
+            if (url.IsNullOrWhiteSpace()) return;
+            var requestUrl = $"{url}/api/abp/application-configuration";
+            new HttpClient().GetAsync(requestUrl).Wait();
         }
     }
 }
