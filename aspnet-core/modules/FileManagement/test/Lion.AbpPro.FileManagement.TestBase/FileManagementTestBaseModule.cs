71050ddcbcfa755e6011537e8874305b49f44a44
王军 <510423039@qq.com>
2023-02-06 22:00:30 +0800

    feat: 单元测试取消本地事件和分布式事件

--- aspnet-core/modules/FileManagement/test/Lion.AbpPro.FileManagement.TestBase/FileManagementTestBaseModule.cs
+++ aspnet-core/modules/FileManagement/test/Lion.AbpPro.FileManagement.TestBase/FileManagementTestBaseModule.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.EventBus.Local;
+
 namespace Lion.AbpPro.FileManagement;
 
 [DependsOn(
@@ -10,6 +14,10 @@ public class FileManagementTestBaseModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
+        // 单元测试取消本地事件
+        context.Services.Replace(ServiceDescriptor.Singleton<ILocalEventBus>(NullLocalEventBus.Instance));
+        // 单元测试取消集成事件
+        context.Services.Replace(ServiceDescriptor.Singleton<IDistributedEventBus>(NullDistributedEventBus.Instance));
         context.Services.AddAlwaysAllowAuthorization();
     }
 
