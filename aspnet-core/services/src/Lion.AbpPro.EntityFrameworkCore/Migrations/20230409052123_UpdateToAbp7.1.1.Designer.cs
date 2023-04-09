61de47bb9faa14e0deb9ccc6f5e44cfbe611aff2
王军 <510423039@qq.com>
2023-04-09 13:26:28 +0800

    chore: 升级7.1.1

--- aspnet-core/services/src/Lion.AbpPro.EntityFrameworkCore/Migrations/20230409052123_UpdateToAbp7.1.1.Designer.cs
+++ aspnet-core/services/src/Lion.AbpPro.EntityFrameworkCore/Migrations/20230409052123_UpdateToAbp7.1.1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Lion.AbpPro.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Lion.AbpPro.Migrations
 {
     [DbContext(typeof(AbpProDbContext))]
-    partial class AbpProDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230409052123_UpdateToAbp7.1.1")]
+    partial class UpdateToAbp711
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
