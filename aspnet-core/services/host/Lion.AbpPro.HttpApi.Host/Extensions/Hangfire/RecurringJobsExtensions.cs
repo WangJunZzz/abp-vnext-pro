e31801af57286e245e3b2c96ae2cdcd044901331
王军 <510423039@qq.com>
2023-05-24 20:29:06 +0800

    fix: 调整Job创建方式

--- aspnet-core/services/host/Lion.AbpPro.HttpApi.Host/Extensions/Hangfire/RecurringJobsExtensions.cs
+++ aspnet-core/services/host/Lion.AbpPro.HttpApi.Host/Extensions/Hangfire/RecurringJobsExtensions.cs
@@ -6,9 +6,7 @@ namespace Lion.AbpPro.Extensions.Hangfire
     {
         public static void CreateRecurringJob(this ApplicationInitializationContext context)
         {
-            using var scope = context.ServiceProvider.CreateScope();
-            var testJob = scope.ServiceProvider.GetService<TestJob>();
-            RecurringJob.AddOrUpdate("测试Job", () => testJob.ExecuteAsync(), CronType.Minute(1), TimeZoneInfo.Local);
+            RecurringJob.AddOrUpdate<TestJob>("测试Job", t => t.ExecuteAsync(), CronType.Minute(1),TimeZoneInfo.Local);
         }
     }
 }
