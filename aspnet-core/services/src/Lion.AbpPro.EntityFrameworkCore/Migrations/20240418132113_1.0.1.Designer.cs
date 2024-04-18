1ce32cc4d9fa84187f1b00531a903bec2a776567
WangJunZzz <510423039@qq.com>
2024-04-18 21:26:50 +0800

    feat: 升级abp8.1.1

--- aspnet-core/services/src/Lion.AbpPro.EntityFrameworkCore/Migrations/20240418132113_1.0.1.Designer.cs
+++ aspnet-core/services/src/Lion.AbpPro.EntityFrameworkCore/Migrations/20240418132113_1.0.1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Lion.AbpPro.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Lion.AbpPro.Migrations
 {
     [DbContext(typeof(AbpProDbContext))]
-    partial class AbpProDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240418132113_1.0.1")]
+    partial class _101
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
