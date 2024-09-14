fc4c5bd3909226b2717b892a7c6c23382a8187e3
WangJunZzz <510423039@qq.com>
2024-09-14 15:44:52 +0800

    feat: 集成WebApiClientCore

---
+++ aspnet-core/frameworks/src/Lion.AbpPro.HttpClient/Lion/AbpPro/HttpClient/IDemoHttpClient.cs
@@ -0,0 +1,20 @@
+﻿using WebApiClientCore;
+using WebApiClientCore.Attributes;
+
+namespace Lion.AbpPro.HttpClient;
+
+/// <summary>
+/// Demo
+/// </summary>
+public interface IDemoHttpClient : IHttpApi
+{
+    /// <summary>
+    /// 获取天气信息
+    /// </summary>
+    [HttpGet("api/WeatherForecast/Get")]
+    Task<IEnumerable<WeatherForecast>> GetAsync(CancellationToken token = default);
+
+
+    [HttpPost("api/WeatherForecast/Demo1")]
+    Task<IEnumerable<WeatherForecast>> PostAsync([JsonContent] WeatherForecast weatherForecasts, CancellationToken token = default);
+}
---
+++ aspnet-core/frameworks/src/Lion.AbpPro.HttpClient/Lion/AbpPro/HttpClient/SevenHttpClientModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace Lion.AbpPro.HttpClient;
+
+[DependsOn(typeof(AbpAutofacModule))]
+public class SevenHttpClientModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        // 示例项目
+        context.Services.AddHttpApi<IDemoHttpClient>().ConfigureHttpApi(options => { options.HttpHost = new Uri("http://localhost:5048/"); });
+    }
+}
