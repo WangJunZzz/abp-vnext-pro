aa970b8c3495e24d6b2514ab89b6dacf95aa8675
hanpaopao <510423039@qq.com>
2024-11-15 17:09:06 +0800

    fix: 调整多语言provider

--- aspnet-core/shared/Lion.AbpPro.Shared.Hosting.Microservices/Microsoft/AspNetCore/Localization/AbpProAcceptLanguageHeaderRequestCultureProvider.cs
+++ aspnet-core/shared/Lion.AbpPro.Shared.Hosting.Microservices/Microsoft/AspNetCore/Localization/AbpProAcceptLanguageHeaderRequestCultureProvider.cs
@@ -13,7 +13,7 @@ public class AbpProAcceptLanguageHeaderRequestCultureProvider : AcceptLanguageHe
             var culture = result.Cultures.First();
 
             // 判断是否以 zh-CN(浏览器默认),zh-HK(中国香港),zh-MO(中国澳门),zh-TW(中国台湾)区域开头,如果是一律采用简体中文
-            if (culture.Buffer != null && (culture.Buffer.StartsWith("zh-CN") || culture.Buffer.StartsWith("zh-HK") || culture.Buffer.StartsWith("zh-TW") || culture.Buffer.StartsWith("zh-MO")))
+            if (culture.Buffer != null && (culture.Buffer.StartsWith("zh-CN") || culture.Buffer.StartsWith("zh-cn") || culture.Buffer.StartsWith("zh-HK") || culture.Buffer.StartsWith("zh-TW") || culture.Buffer.StartsWith("zh-MO")))
             {
                 culture = new StringSegment("zh-Hans");
                 return new ProviderCultureResult(culture.Buffer, culture.Buffer);
