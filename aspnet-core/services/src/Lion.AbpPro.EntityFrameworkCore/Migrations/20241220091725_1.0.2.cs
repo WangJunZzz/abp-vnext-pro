4468d31125d025b106a76962d0e05d8a893c4c3c
Hanpaopao <510423039@qq.com>
2024-12-20 19:24:32 +0800

    feat: 支持多租户

---
+++ aspnet-core/services/src/Lion.AbpPro.EntityFrameworkCore/Migrations/20241220091725_1.0.2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lion.AbpPro.Migrations
+{
+    /// <inheritdoc />
+    public partial class _102 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SupportTenant",
+                table: "AbpProProjects",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SupportTenant",
+                table: "AbpProProjects");
+        }
+    }
+}
