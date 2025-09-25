ed9eb9abbde4094352d36ad21bba58e4afc27856
zzzwangjun@gmail.com <510423039@qq.com>
2025-09-25 15:38:01 +0800

    feat: 调整模块模板

---
+++ templates/pro-module/host/MyCompanyName.MyProjectName.MyModuleName.HttpApi.Host/Microsoft/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MyCompanyName.MyProjectName.MyModuleName;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// 注册Redis缓存
+    /// </summary>
+    public static IServiceCollection AddAbpProRedis(this IServiceCollection service)
+    {
+        service.Configure<AbpDistributedCacheOptions>(options => { options.KeyPrefix = "AbpPro:"; });
+        var configuration = service.GetConfiguration();
+        var redis = ConnectionMultiplexer.Connect(configuration.GetValue<string>("Redis:Configuration"));
+        service
+            .AddDataProtection()
+            .PersistKeysToStackExchangeRedis(redis, "AbpPro-Protection-Keys");
+        return service;
+    }
+    /// <summary>
+    /// 配置虚拟文件系统
+    /// </summary>
+    public static IServiceCollection AddAbpProVirtualFileSystem(this IServiceCollection service)
+    {
+        service.Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<MyModuleNameHttpApiHostModule>();
+        });
+        return service;
+    }
+    
+    public static IServiceCollection AddAbpProDbContext(this IServiceCollection service)
+    {
+        service.Configure<AbpDbContextOptions>(options => { options.UseMySQL(builder => { builder.TranslateParameterizedCollectionsToConstants(); }); });
+        return service;
+    }
+
+    
+}
